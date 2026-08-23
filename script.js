dae504daff878167089c08d5dc4af6bd6d0e638c
Abdo Tarek <zizotarek553@gmail.com>
2026-08-23 10:09:34 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(".faq-item__q").forEach(function (button) {
-  button.addEventListener("click", function () {
-    button.parentElement.classList.toggle("is-open");
-  });
-});
